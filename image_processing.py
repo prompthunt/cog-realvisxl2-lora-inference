f095f7a015feea2ab49576bd895072dee576f189
Faruk Alibasic <faruk.alibasic@gmail.com>
2023-11-13 14:39:29 +0300

    fix head_mask.save

--- image_processing.py
+++ image_processing.py
@@ -318,4 +318,4 @@ def get_head_mask(
 
     # Apply blur amount
     pil_image_blur = pil_image.filter(ImageFilter.GaussianBlur(blur_amount))
-    return pil_image_blur, pil_image
+    return pil_image_blur
